691b1cf8c75e4bd156b0dea256d174b81b4b05c6
Tapan Sharma <tapansharma69@gmail.com>
2024-01-31 22:07:10 +0545

    setup article and episode models and configure them

---
+++ app/Http/Controllers/EpisodeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Episode;
+use Illuminate\Http\Request;
+
+class EpisodeController extends Controller
+{
+    public function __invoke(Episode $episode)
+    {
+        return view('episodes.show', compact('episode'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\EpisodeController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -26,6 +17,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/articles/{article:slug}', ArticleController::class)->name('articles.show');
+    Route::get('/episodes/{episode:slug}', EpisodeController::class)->name('episodes.show');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
