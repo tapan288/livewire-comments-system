904e6b0f4ba438a93a7d7f6eebae407c6ca3413c
Tapan Sharma <tapansharma69@gmail.com>
2024-02-01 21:47:06 +0545

    listing the replies

--- resources/views/livewire/comment.blade.php
+++ resources/views/livewire/comment.blade.php
@@ -18,15 +18,17 @@
         </p>
         <!--  Reply,Edit, Delete Section -->
         <div class="flex items-center mt-4 space-x-4">
-            <button type="button" class="flex items-center text-sm text-gray-500 hover:underline">
-                <svg aria-hidden="true" class="mr-1 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z">
-                    </path>
-                </svg>
-                Reply
-            </button>
+            @if (!$comment->parent_id)
+                <button type="button" class="flex items-center text-sm text-gray-500 hover:underline">
+                    <svg aria-hidden="true" class="mr-1 w-4 h-4" fill="none" stroke="currentColor"
+                        viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z">
+                        </path>
+                    </svg>
+                    Reply
+                </button>
+            @endif
             <button type="button" class="flex items-center text-sm text-gray-500 hover:underline">
                 <svg class="mr-1 w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                     stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
@@ -44,21 +46,11 @@
                 Delete
             </button>
         </div>
-        {{-- <form class="mb-6 mt-6 ml-6" wire:submit="replyComment">
-                    <div class="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200">
-                        <label for="comment" class="sr-only">Your comment</label>
-                        <textarea id="comment" style="resize: none;" placeholder="Write a comment..."
-                            class="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none
-                                "></textarea>
-                    </div>
-                    <button type="submit"
-                        class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-primary-200 hover:bg-blue-800">
-                        Reply
-                    </button>
-                    <button
-                        class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center border-gray-300  text-gray-700 bg-white hover:bg-gray-50 rounded-lg">
-                        Cancel
-                    </button>
-                </form> --}}
     </article>
+
+    <div class="ml-8 mt-6">
+        @foreach ($comment->replies as $reply)
+            @livewire('comment', ['comment' => $reply], key($reply->id))
+        @endforeach
+    </div>
 </div>
