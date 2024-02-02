dfac89f13b83572a7ec3e208ae7e0f513c260b71
Tapan Sharma <tapansharma69@gmail.com>
2024-02-02 22:57:12 +0545

    implement eager loading

--- app/Livewire/Comments.php
+++ app/Livewire/Comments.php
@@ -20,7 +20,12 @@ class Comments extends Component
     public function render()
     {
         return view('livewire.comments', [
-            'comments' => $this->model->comments()->parent()->latest()->get(),
+            'comments' => $this->model
+                ->comments()
+                ->with('user', 'replies.user', 'replies.replies')
+                ->parent()
+                ->latest()
+                ->get(),
         ]);
     }
 }
